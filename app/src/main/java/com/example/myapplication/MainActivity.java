b35ac3b34b481a8063adbd64a69b40e26e178642
yangfei1 <yangfei1@thundersoft.com>
2021-02-08 17:17:39 +0800

    “修改布局样式”

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,14 +3,18 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView hello;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
+        hello = findViewById(R.id.hello);
+        hello.setText("真好");
     }
 }
